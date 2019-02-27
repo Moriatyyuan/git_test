ae84a89aed36bea2cdff46984447a028eb7bed79
Moriaty <moriatyyuan@gmail.com>
2019-02-28 02:28:55 +0900

    1

---
+++ martix.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([[0.92, 0.39], [0.39, -0.92]])
+b = np.array([[3.12, 1.3], [1.3, 0.58]])
+c = np.array([[0.92, 0.39], [0.39, -0.92]])
+d = np.array([[1, 0.427], [-2.377, 1]])
+e = np.transpose(d)
+print(np.dot(e, d))
